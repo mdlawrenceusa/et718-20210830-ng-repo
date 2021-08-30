896dbc40ff42d2dfe4e5e5cebcc2623a69b6c57b
mdlawrenceusa <mdlawrence@me.com>
2021-08-30 11:54:21 -0400

    4 files modified, 1 added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -10,6 +10,9 @@ import { Product } from '../products';
 })
 export class ProductAlertsComponent implements OnInit {
 
+  @Input() product: Product|undefined;
+  @Output() notify = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
